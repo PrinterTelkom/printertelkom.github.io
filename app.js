9ce0467cb6479b1403d7290a482c8bff8760059d
PrinterTelkom <valentinusaldositumorang@gmail.com>
2026-09-20 22:35:04 +0700

    Auto-sync: Hubungkan app.js ke tunnel https://blogs-consequence-responsible-strongly.trycloudflare.com

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Backend Cloudflare Tunnel URL (Auto-Updated)
-const SERVER_URL = 'https://dollar-oliver-collar-filter.trycloudflare.com';
+const SERVER_URL = 'https://blogs-consequence-responsible-strongly.trycloudflare.com';
 
 // Inisialisasi PDF.js Worker untuk hitung halaman di Client
 if (window.pdfjsLib) {

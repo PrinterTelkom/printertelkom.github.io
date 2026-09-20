9cca1627c6b8b286ba63d34f24c594a8661763cf
PrinterTelkom <valentinusaldositumorang@gmail.com>
2026-09-20 22:20:19 +0700

    Auto-sync: Hubungkan app.js ke tunnel https://this-sign-deemed-panels.trycloudflare.com

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Backend Cloudflare Tunnel URL (Auto-Updated)
-const SERVER_URL = 'https://giving-blanket-psychiatry-invitations.trycloudflare.com';
+const SERVER_URL = 'https://this-sign-deemed-panels.trycloudflare.com';
 
 // Inisialisasi PDF.js Worker untuk hitung halaman di Client
 if (window.pdfjsLib) {

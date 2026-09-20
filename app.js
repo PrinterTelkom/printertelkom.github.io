2740385be43d63dccc0bcec69efc0c6bdf36955d
PrinterTelkom <valentinusaldositumorang@gmail.com>
2026-09-20 22:17:21 +0700

    Auto-sync: Hubungkan app.js ke tunnel https://ongoing-fee-lotus-dropped.trycloudflare.com

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Backend Cloudflare Tunnel URL (Auto-Updated)
-const SERVER_URL = 'https://embedded-took-presents-dear.trycloudflare.com';
+const SERVER_URL = 'https://ongoing-fee-lotus-dropped.trycloudflare.com';
 
 // Inisialisasi PDF.js Worker untuk hitung halaman di Client
 if (window.pdfjsLib) {

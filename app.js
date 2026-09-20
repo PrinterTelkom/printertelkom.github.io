ccef5354c617376321ac9a4370a7db8527066aa4
PrinterTelkom <valentinusaldositumorang@gmail.com>
2026-09-20 20:06:05 +0700

    Update tunnel to https://feeds-champagne-bell-altered.trycloudflare.com

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (!API_BASE_URL) {
     API_BASE_URL = window.location.origin;
   } else {
     // Default fallback saat dibuka di GitHub Pages
-    API_BASE_URL = 'https://southampton-weekend-packages-rca.trycloudflare.com';
+    API_BASE_URL = 'https://feeds-champagne-bell-altered.trycloudflare.com';
   }
 }
 
